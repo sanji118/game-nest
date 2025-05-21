d3c4092c02d99e59d0b67fee915ab23a8e81105e
sanji118 <sanjishifa32@gmail.com>
2025-05-21 14:28:37 +0600

    added theme controller

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { IoMenu } from "react-icons/io5"
+import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    {/**desktop Navbar */}
+    <div class="navbar bg-base-100 shadow-sm">
+        <div class="flex-1">
+            <img src="https://i.ibb.co/FLXmmxqR/game-logo.jpg" alt="GameNest-logo" className=""/>
+        </div>
+        <div class="flex-none">
+            <div>
+                <NavLink to={'/'}>Home</NavLink>
+                <NavLink to={'/reviews'}>All Reviews</NavLink>
+            </div>
+        </div>
+    </div>
+    </>
   )
 }
 
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Banner from '../components/HomePage/Banner'
+import Footer from '../components/Footer'
 
 const MainLayout = () => {
   return (
-    <div>MainLayout</div>
+    <div>
+      <Navbar></Navbar>
+      <Banner></Banner>
+      <Footer></Footer>
+    </div>
   )
 }
 
